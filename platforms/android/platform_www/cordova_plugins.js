8d24ac38e0a63336dfe30887b66f5e5a5470a63a
John Colagioia <john@colagioia.net>
2017-12-19 06:38:27 -0500

    Add curl library

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "ru.apps-m.curl.curl",
+    "file": "plugins/ru.apps-m.curl/www/curl.js",
+    "pluginId": "ru.apps-m.curl",
+    "clobbers": [
+      "curl"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "ru.apps-m.curl": "0.2.0"
 };
 // BOTTOM OF METADATA
 });
